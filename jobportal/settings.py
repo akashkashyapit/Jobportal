da8ea0ce436cff267179acbccfad0ff87356a4ed
akash <akashkashyapit@gmail.com>
2020-02-05 20:44:58 +0530

    second commit

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -1,8 +1,5 @@
 import os
-import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,12 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
